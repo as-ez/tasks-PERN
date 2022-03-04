719dfb1c6ff830b7a2391a909911449c181a4f7f
astradae <astradaezequiel.tpi@gmail.com>
2022-03-04 17:53:34 -0300

    Metodo DELETE - Eliminar Tarea

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -10,7 +10,6 @@ const getAllTasks = async (req, res) => {
 };
 
 const getTask = async (req, res) => {
-  
   try {
     const { id } = req.params;
     const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
@@ -38,8 +37,17 @@ const createTask = async (req, res) => {
   }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Hola");
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await pool.query("DELETE FROM task WHERE id = $1", [id]);
+
+  if (result.rows.length === 0)
+    return res.status(404).json({
+      message: "Task not found",
+    });
+
+  return res.sendStatus(204);
 };
 
 const updateTask = (req, res) => {
