1cddcdc80995cf6482aad27bf07e9aaf58556a11
astradae <astradaezequiel.tpi@gmail.com>
2022-03-04 15:43:53 -0300

    Endpoints

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const rutas = require('./routes/tasks.routes')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(rutas)
 
 app.listen(3000)
 console.log('Server on port 3000')
---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,25 @@
+const {Router} = require('express')
+
+const router = Router();
+
+router.get('/tasks', (req, res) => {
+    res.send('Hola')
+})
+
+router.get('/tasks/:id', (req, res) => {
+    res.send('Hola')
+})
+
+router.post('/tasks', (req, res) => {
+    res.send('Hola')
+})
+
+router.delete('/tasks', (req, res) => {
+    res.send('Hola')
+})
+
+router.put('/tasks', (req, res) => {
+    res.send('Hola')
+})
+
+module.exports = router
