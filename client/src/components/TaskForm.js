0dca1a0c62ea9f8aa6d7ea3e036777996af94af8
astradae <astradaezequiel.tpi@gmail.com>
2022-03-05 19:30:34 -0300

    Eliminar Tarea desde React y Actualizar Tarea desde React

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -8,7 +8,7 @@ import {
   Typography,
 } from "@mui/material";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function TaskForm() {
   const [task, setTask] = useState({
@@ -17,22 +17,28 @@ export default function TaskForm() {
   });
 
   const [loading, setLoading] = useState(false);
+  const [editing, setEditing] = useState(false);
 
   const navigate = useNavigate();
+  const params = useParams();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     setLoading(true);
 
-    const res = await fetch("http://localhost:4000/tasks", {
-      method: "POST",
-      body: JSON.stringify(task),
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await res.json();
-    console.log(data);
-
+    if (editing) {
+      await fetch(`http://localhost:4000/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+      })
+    } else {
+      await fetch("http://localhost:4000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+      });
+    }
     setLoading(false);
     navigate("/");
   };
@@ -40,6 +46,20 @@ export default function TaskForm() {
   const handleChange = (e) => {
     setTask({ ...task, [e.target.name]: e.target.value });
   };
+
+  const loadTask = async (id) => {
+    const res = await fetch(`http://localhost:4000/tasks/${id}`);
+    const data = await res.json();
+    setTask({ title: data.title, description: data.description });
+    setEditing(true);
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      loadTask(params.id);
+    }
+  }, [params.id]);
+
   return (
     <Grid
       container
@@ -56,7 +76,7 @@ export default function TaskForm() {
           }}
         >
           <Typography variant="5" textAlign="center" color="white">
-            Create Task{" "}
+            {editing ? "Edit Task" : "Add Task"}
           </Typography>
           <CardContent>
             <form onSubmit={handleSubmit}>
@@ -68,12 +88,14 @@ export default function TaskForm() {
                   margin: ".5rem 0",
                 }}
                 name="title"
+                value={task.title}
                 onChange={handleChange}
                 inputProps={{ style: { color: "white" } }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
               <TextField
                 variant="filled"
+                value={task.description}
                 label="Write your description"
                 multiline
                 rows={4}
@@ -94,7 +116,7 @@ export default function TaskForm() {
                 {loading ? (
                   <CircularProgress color="inherit" size={24} />
                 ) : (
-                  "Create"
+                  "Save"
                 )}
               </Button>
             </form>
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
 import { Button, Card, CardContent, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function TaskList() {
   const [tasks, setTasks] = useState([]);
+
+  const navigate = useNavigate()
 
   const loadTask = async () => {
     const response = await fetch("http://localhost:4000/tasks");
     const data = await response.json();
     setTasks(data);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:4000/tasks/${id}`, {
+        method: "DELETE",
+      });
+
+      setTasks(tasks.filter((task) => task.id !== id));
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
@@ -23,6 +38,7 @@ export default function TaskList() {
             marginBottom: ".7rem",
             backgroundColor: "#1e272e",
           }}
+          key={task.id}
         >
           <CardContent
             style={{
@@ -39,14 +55,14 @@ export default function TaskList() {
               <Button
                 variant="contained"
                 color="inherit"
-                onClick={() => console.log("edit")}
+                onClick={() => navigate(`/tasks/${task.id}/edit`)}
               >
                 Edit
               </Button>
               <Button
                 variant="contained"
                 color="warning"
-                onClick={() => console.log("delete")}
+                onClick={() => handleDelete(task.id)}
                 style={{ marginLeft: ".5rem" }}
               >
                 Delete
