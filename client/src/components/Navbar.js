7d1175b7037300585c339519983968469e311174
astradae <astradaezequiel.tpi@gmail.com>
2022-03-05 02:02:16 -0300

    Material UI

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,10 @@
+import { Button } from "@mui/material";
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      Navbar
+      <Button variant='contained' color='primary'>Click Me</Button>
+    </div>
+  );
 }
